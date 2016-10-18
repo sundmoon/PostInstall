e9d6f65d30a8b36ef73d1fca83f399baa72fb7d3
Lev Tikhonov <sundmoon@gmail.com>
2016-10-18 22:19:27 +0300

    1st try

---
+++ src/public/LtPostInstall.ps1
@@ -0,0 +1,41 @@
+﻿
+# Save ConfigurationData in a file with .psd1 file extension
+
+
+configuration LtPostInstall
+{
+  Import-DscResource -ModuleName 'PSDesiredStateConfiguration', 'Carbon'
+  
+  Import-DscResource -Name SystemLocale, xTimeZone, xRemoteDesktopAdmin
+
+  # One can evaluate expressions to get the node list
+  # E.g: $AllNodes.Where("Role -eq Web").NodeName
+  node 'localhost'
+  {
+    # Call Resource Provider
+    # E.g: WindowsFeature, File
+
+    xRemoteDesktopAdmin EnableRDAdmin
+    {
+      Ensure = 'Present' 
+      UserAuthentication = 'secure'
+    }
+
+    Carbon_firewallRule EnableRDPIn
+    {
+      Name = 'Remote Desktop - User Mode (TCP-In)'    
+      Enabled = $true
+      Ensure = 'Present'
+      Profile = 'Domain', 'Private'
+    }
+
+    {
+      Name = 'Remote Desktop - User Mode (UDP-In)'    
+      Enabled = $true
+      Ensure = 'Present'
+      Profile = 'Domain', 'Private'
+    }
+  }
+}
+
+# ConfigurationName -configurationData <path to ConfigurationData (.psd1) file>
---
+++ src/public/LtPostInstall.psd1
@@ -0,0 +1,32 @@
+﻿
+@{
+  AllNodes    = @(
+    @{
+      NodeName = 'localhost'
+      Locale   = 'ru-RU'
+      TimeZone = 'Russian Standard Time'
+    }
+  )
+  NonNodeData = @{
+    ConfigFileContents = 'this is contents'
+  }
+}
+
+<#
+@{
+    AllNodes = @(
+        @{
+            NodeName = 'Node1'
+            Role = 'WebServer'
+            },
+        @{
+            NodeName = 'Node2'
+            Role = 'SQLServer'
+            },
+        @{
+            NodeName = 'Node3'
+            Role = 'WebServer'
+            }
+    )
+}
+#>
